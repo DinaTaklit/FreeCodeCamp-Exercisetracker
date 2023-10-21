f0929711ac0620c2f78f8f3f3ec20dfbbdf177a6
Taklit Dina <ez_taklit@esi.dz>
2023-10-21 12:02:54 +0100

    feat: get list of logs per user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const { isValid, parseISO } = require("date-fns");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -13,7 +13,6 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
 
-// connect to database
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -45,6 +44,7 @@ let exerciseSchema = new mongoose.Schema({
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 // utils functions
+
 function formatDate(inputDate) {
   const date = new Date(inputDate);
 
@@ -85,6 +85,7 @@ app.get("/api/users", async (req, res) => {
 app.post("/api/users/:_id/exercises", async (req, res) => {
   const id = req.params._id;
   const { description, duration, date } = req.body;
+
   try {
     const user = await User.findById(id);
     if (!user) {
@@ -104,6 +105,61 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
       username: user.username,
       ...exerciseObj,
       _id: id,
+    };
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/api/users/:_id/logs", async (req, res) => {
+  const id = req.params._id;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = parseInt(req.query.limit) || null;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const query = {};
+    if (from) {
+      if (!isValid(parseISO(from))) {
+        return res.status(400).json({ error: "Invalid from date format" });
+      }
+      query.date = { $gte: new Date(from) };
+    }
+    if (to) {
+      if (!isValid(parseISO(to))) {
+        return res.status(400).json({ error: "Invalid to date format" });
+      }
+      if (query.date) {
+        query.date.$lte = new Date(to);
+      } else {
+        query.date = { $lte: new Date(to) };
+      }
+    }
+
+    const exercisesRes = await user.populate({
+      path: "exercises",
+      match: query,
+      options: { limit },
+    });
+    const exercises = exercisesRes.exercises;
+    if (!exercises || !exercises.length) {
+      return res.status(404).json({ error: "Exercies not found" });
+    }
+    const response = {
+      username: user.username,
+      count: exercises.length,
+      _id: id,
+      log: exercises.map((exercise) => ({
+        description: exercise.description,
+        duration: exercise.duration,
+        date: formatDate(exercise.date).replace(/,/g, ""),
+      })),
     };
     res.json(response);
   } catch (err) {
